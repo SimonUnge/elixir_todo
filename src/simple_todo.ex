92147bd9cd8c9f38f4d4232b435cb81776b0f81c
Simon Unge <sunge@cisco.com>
2019-04-04 22:07:31 -0700

    4.2.4

--- src/simple_todo.ex
+++ src/simple_todo.ex
@@ -1,18 +1,21 @@
 defmodule TodoList do
   defstruct auto_id: 1, entries: %{}
 
-  def new(), do: %TodoList{}
+  def new() do
+    %TodoList{}
+  end
 
   def add_entry(todo_list, entry) do
     entry = Map.put(entry, :id, todo_list.auto_id)
     new_entries = Map.put(
       todo_list.entries,
       todo_list.auto_id,
-      entry)
+      entry
+    )
 
     %TodoList{todo_list |
-              entries: new_entries,
-              auto_id: todo_list.auto_id + 1
+              auto_id: todo_list.auto_id + 1,
+              entries: new_entries
     }
   end
 
@@ -22,4 +25,18 @@ defmodule TodoList do
     |> Enum.map(fn {_, entry} -> entry end)
   end
 
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+      {:ok, old_entry} ->
+        new_entry = %{id: ^old_entry.id} =updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, entry_id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
+  end
 end
