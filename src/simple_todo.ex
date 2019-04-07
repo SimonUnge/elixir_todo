c3705571d8c727188a8f98c022c3647b24072e3c
Simon Unge <sunge@cisco.com>
2019-04-07 10:35:14 -0700

    read from file

--- src/simple_todo.ex
+++ src/simple_todo.ex
@@ -1,8 +1,12 @@
 defmodule TodoList do
   defstruct auto_id: 1, entries: %{}
 
-  def new() do
-    %TodoList{}
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      &add_entry(&2, &1)
+    )
   end
 
   def add_entry(todo_list, entry) do
@@ -39,5 +43,51 @@ defmodule TodoList do
 
   def delete_entry(todo_list, entry_id) do
     %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
+  end
+
+  def tmp do
+    [
+      %{date: ~D[2018-12-19], title: "Dentist"},
+      %{date: ~D[2018-12-20], title: "Shopping"},
+      %{date: ~D[2018-12-19], title: "Movies"}
+    ]
+  end
+end
+
+defmodule TodoList.CsvImporter do
+  def import(file) do
+    file
+    |> read_lines
+    |> create_entries
+    |> TodoList.new
+  end
+
+  defp read_lines(file) do
+    File.stream!(file)
+    |> Stream.map(&String.replace(&1, "\n", ""))
+  end
+
+  defp create_entries(lines) do
+    lines
+    |> Stream.map(&extract_fields/1)
+    |> Stream.map(&create_entry/1)
+  end
+
+  defp extract_fields(line) do
+    [date, title] = String.split(line, ",")
+    {create_date(date), title}
+  end
+
+  defp create_date(date) do
+    [y,m,d] =
+      date
+      |> String.split("/")
+      |> Enum.map(&String.to_integer/1)
+    {:ok, date} = Date.new(y,m,d);
+    date
+  end
+
+  defp create_entry({date, title}) do
+    %{date: date, title: title}
   end
 end
