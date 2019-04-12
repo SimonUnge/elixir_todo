928e684a6ce2a0e99f8676215761113d4d16594e
Simon Unge <sunge@cisco.com>
2019-04-12 15:15:14 -0700

    to string and collectable implemented

--- lib/todo.ex
+++ lib/todo.ex
@@ -82,3 +82,27 @@ defmodule Todo.CsvImporter do
     %{date: date, title: title}
   end
 end
+
+defimpl String.Chars, for: Todo do
+  def to_string(_) do
+    "#TodoFubah"
+  end
+end
+
+defimpl Collectable, for: Todo do
+  def into(original) do
+    {original, &into_callback/2}
+  end
+
+  defp into_callback(todo_list, {:cont, entry}) do
+    Todo.add_entry(todo_list, entry)
+  end
+
+  defp into_callback(todo_list, :done) do
+    todo_list
+  end
+
+  defp into_callback(todo_list, :halt) do
+    :ok
+  end
+end
